bdc891d48f1d8f1b3b31afafd110d3cef402c5c4
Liu <liu9708@icloud.com>
2023-04-27 21:48:14 +0800

    20230427

--- 44-SmartPointer/44-SP_2.cpp
+++ 44-SmartPointer/44-SP_2.cpp
@@ -1,138 +1,68 @@
 #include <iostream>
 using namespace std;
 /*
-1-传统指针存在的问题
-1.1-需要手动管理内存 
-1.2-容易发生内存泄露(忘记释放、出现异常等) 
-1.3-释放之后产生野指针
-2-智能指针就是为了解决传统指针存在的问题 
-2.1-auto_ptr:属于C++98标准，在C++11中已经不推荐使用(有缺陷，比如不能用于数组) 
-2.2-shared_ptr:属于C++11标准
-2.3-unique_ptr:属于C++11标准
-*/
+1-shared_ptr的设计理念 
+1.1-多个shared_ptr可以指向同一个对象，当最后一个shared_ptr在作用域范围内结束时，对象才会被自动释放
+1.2-可以通过一个已存在的智能指针初始化一个新的智能指针
+2-shared_ptr的原理
+2.1一个shared_ptr会对一个对象产生强引用(strong reference)
+2.2-每个对象都有个与之对应的强引用计数，记录着当前对象被多少个shared_ptr强引用着
+    2.2.1-可以通过shared_ptr的use_count函数获得强引用计数
+    2.2.2-当有一个新的shared_ptr指向对象时，对象的强引用计数就会+1
+    2.2.3-当有一个shared_ptr销毁时(比如作用域结束)，对象的强引用计数就会-1
+    2.2.4-当一个对象的强引用计数为0时(没有任何shared_ptr指向对象时)，对象就会自动销毁(析构)
+*/ 
 
-// class Person {
-// 	int m_age;
-// public:
-// 	Person() {
-// 		cout << "Person()" << endl;
-// 	}
-// 	Person(int age) :m_age(age) {
-// 		cout << "Person(int)" << endl;
-// 	}
-// 	~Person() {
-// 		cout << "~Person()" << endl;
-// 	}
-// 	void run() {
-// 		cout << "run() - " << m_age << endl;
-// 	}
-// };
+class Person {
+	int m_age;
+public:
+	Person() {
+		cout << "Person()" << endl;
+	}
+	Person(int age) :m_age(age) {
+		cout << "Person(int)" << endl;
+	}
+	~Person() {
+		cout << "~Person()" << endl;
+	}
+	void run() {
+		cout << "run() - " << m_age << endl;
+	}
+};
 
-// template <typename T>
-// class SmartPointer {
-// private:
-// 	T *m_obj;
-// public:
-// 	SmartPointer(T *obj) :m_obj(obj) {}
-// 	~SmartPointer() {
-// 		if (m_obj == nullptr) return;
-// 		delete m_obj;
-// 	}
+int main() {
 
-// 	T *operator->() { // 运算符重载
-// 		return m_obj;
-// 	}
-// };
+    // 1.1
+    // cout << 1 << endl;
+	// {
+    //     shared_ptr<Person> p4;
+    //     {
+    //         shared_ptr<Person> p1(new Person(10));
+    //         shared_ptr<Person> p2 = p1;
+    //         shared_ptr<Person> p3 = p2;
+    //         p4 = p3;
+    //     }
+	// 	cout << 2 << endl;
+	// }
+    // cout << 3 << endl;
 
-// void test() {
-// 	cout << 1 << endl;
-// 	{
-// 		auto_ptr<Person> p(new Person(20)); // c++98
-// 		p->run();
-// 	}
-// 	cout << 2 << endl;
-// }
+    // 2.2
+	{
+        shared_ptr<Person> p4;
+        {
+            shared_ptr<Person> p1(new Person(10));
+            cout << p1.use_count() << endl;
 
-//void test2() {
-//	cout << 1 << endl;
-//	{
-//		SmartPointer<Person> p(new Person(20));
-//		p->run();
-//	}
-//	cout << 2 << endl;
-//}
-//
-//void test3() {
-//	{
-//		shared_ptr<Person> p4;
-//		{
-//			shared_ptr<Person> p1(new Person(10));
-//			cout << p1.use_count() << endl; // 1
-//
-//			shared_ptr<Person> p3;
-//			{
-//				shared_ptr<Person> p2 = p1;
-//				cout << p2.use_count() << endl; // 2
-//
-//				p3 = p2; // 3
-//			} // 2
-//			cout << p3.use_count() << endl; // 2
-//
-//			p4 = p3;
-//			cout << p4.use_count() << endl; // 3
-//		} // 1
-//
-//		cout << p4.use_count() << endl; // 1
-//	}
-//}
+            shared_ptr<Person> p2 = p1;
+            cout << p1.use_count() << endl;
+            
+            shared_ptr<Person> p3 = p2;
+            cout << p1.use_count() << endl;
+            p4 = p3;
+            cout << p1.use_count() << endl;
+        }	
+	}
 
-// class Person;
-
-// class Car {
-// public:
-// 	weak_ptr<Person> m_person;
-// 	Car() {
-// 		cout << "Car()" << endl;
-// 	}
-// 	~Car() {
-// 		cout << "~Car()" << endl;
-// 	}
-// };
-
-// class Person {
-// public:
-// 	shared_ptr<Car> m_car; 
-// 	Person() {
-// 		cout << "Person()" << endl;
-// 	}
-// 	~Person() {
-// 		cout << "~Person()" << endl;
-// 	}
-// };
-
-// int main() {
-//     cout << 1 << endl;
-//     {
-//         SmartPointer<Person> p(new Person(20)); // p是个对象
-//         p->run();
-//     }
-//     cout << 2 << endl;
-
-// 	/*{
-// 		shared_ptr<Person> person(new Person());
-// 		shared_ptr<Car> car(new Car());
-
-// 		person->m_car = car;
-// 		car->m_person = person;
-// 	}
-// */
-
-// 	// unique_ptr<Person> p0;
-// 	// {
-// 	// 	unique_ptr<Person> p1(new Person());
-// 	// 	p0 = std::move(p1);
-// 	// }
-
-// 	// getchar();
-// 	return 0;
-// }
+	// getchar();
+	return 0;
+}
