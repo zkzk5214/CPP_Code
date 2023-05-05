f5d18ef5e656f0b96532da80f6b0d2a2f0fbf6f9
liu <liu9708@icloud.com>
2023-05-05 21:07:32 +0800

    20230505

--- 02-CinCout/02-CinCout.cpp
+++ 02-CinCout/02-CinCout.cpp
@@ -1,6 +1,16 @@
 # include <iostream> 
 using namespace std;
 
+// 1- In C++, use 'cout<< ... <<endl' or "\n" to implement a new line in the terminal.
+// But there is a small difference between the two. When using 'endl', the buffer will be refreshed, 
+// so that the things in the stack will also be refreshed, however, "\n" will not cause refresh, 
+// it will only output newline, and the data in the stack will not change.
+// 2- In addition to doing '\n', 'endl' also calls the 'flush' function to refresh the buffer and write
+// the data in the buffer to a file or screen. In consideration of operating efficiency, it's recommended
+// to use '\n' 
+// 3- cout *lt;< endl; In addition to inserting '\n' into the output stream, it can also flush the output stream
+// 4- One of the factors affecting the inefficiency of program execution is the use of many ‘endl’.
+
 int main(){
     // cout - (“see out”) object to display messages on the computer’s screen.
     // endl - (“end-line”) to instruct cout to start a new line.
@@ -11,13 +21,3 @@ int main(){
     // getchar(); 
     return 0;
 }
-
-// 1- In C++, use 'cout<< ... <<endl' or "\n" to implement a new line in the terminal.
-// But there is a small difference between the two. When using 'endl', the buffer will be refreshed, 
-// so that the things in the stack will also be refreshed, however, "\n" will not cause refresh, 
-// it will only output newline, and the data in the stack will not change.
-// 2- In addition to doing '\n', 'endl' also calls the 'flush' function to refresh the buffer and write
-// the data in the buffer to a file or screen. In consideration of operating efficiency, it's recommended
-// to use '\n' 
-// 3- cout *lt;< endl; In addition to inserting '\n' into the output stream, it can also flush the output stream
-// 4- One of the factors affecting the inefficiency of program execution is the use of many ‘endl’.
--- 03-Overload/03-Overload.cpp
+++ 03-Overload/03-Overload.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
-
 /*
-    C 语言不支持函数重载
-    C++ 支持
+1-The function names of the two functions are exactly the same, but the parameter types/numbers/sequences are different.
+2-The return type is independent of the overload.
+3-When calling a function, the implicit type cast of the actual parameter may cause ambiguity.
+    Which essence adopts name mangline/name decoration technology.
+    C++ compiler will adapt and modify symbol names(such as function names) by default.
+    overloading can generate multiple different function names, and different compilers have different generation rules.
+4-C does not support function overloading.    
+    C++ support.
 */
 
 int sum_(int v1, int v2) {
@@ -22,14 +27,15 @@ void func(double v1, int v2){
     cout << "func(double v1, int v2)" << endl;
 }
 
-// 歧义，二义性
+// Ambiguity
 // int func(){
 //     return 0;
 // }
 // double func(){
 //     return 0;
 // }
-// 下面这种方法可以
+
+// Avaliable
 int func(int a){
     return 0;
 }
@@ -37,55 +43,49 @@ double func(){
     return 0;
 }
 
-// display_int (举例命名，并不是编译器的实际命名)(name decoration)
+// display_int (example naming, not the actual naming of the compiler)(name decoration)
 void display(int a){
     cout << "display(int) -" << a << endl;
 }
-
 // display_long
 void display(long a){
     cout << "display(long) -" << a << endl;
 }
-
 // display_float
 void display(float a){
     cout << "display(float) -" << a << endl;
 }
 
 int main() {
-    // 1-两个函数的函数名完全一样，参数 类型/个数/顺序 不同 
-    // 2-返回值类型与重载无关 
-    // 3-调用函数时，实参的隐式类型转化可能会产生二义性
-    // 本质采用了 name mangling/ name decoration 技术
-    // C++ 编译器默认会对符号名（比如函数名）进行改编、修饰
-    // 重载的时候会生成多个不同的函数名，不同编译器有不同的生成规则
 
     // 1-1)
-    // cout << sum_(10, 20) << endl;
-    // cout << sum_(10, 20, 30) << endl;
+    cout << sum_(10, 20) << endl;
+    cout << sum_(10, 20, 30) << endl;
 
     // 1-2
-    // func(10, 10.5);
-    // func(10.5 ,10);
+    func(10, 10.5);
+    func(10.5 ,10);
 
     // 2 
-    // func(10);
+    func(10);
 
     // 3
-    display(10); // long l = 10; 隐式转换；long l = (long) 10; 显式转换
+    display(10); // long l = 10; Implicit Cast ；long l = (long) 10; Explicit Cast
     display(10L);
-    // display(10.0);
+    // display(10.0); // call to 'display' is ambiguous
 
-    // int: 4byte=32bit -2**31~2**31-1
-    // unsigned: 4byte=32bit 0~2**31-1
-    // long: 4byte=32bit -2**31~2**31-1
-    // short: 2byte=16bit -2**15~2**15-1
-    // int 和 long 在不同平台上的长度可能不一样 int至少16位，long 32 
-    // sizeof(int) <= sizeof(long)
+    /*
+    int: 4byte=32bit -2**31~2**31-1
+    unsigned: 4byte=32bit 0~2**31-1
+    long: 4byte=32bit -2**31~2**31-1
+    short: 2byte=16bit -2**15~2**15-1
+    int and long (The size may be different on different platforms, int>=16, long 32) 
+    sizeof(int) <= sizeof(long)
+    */ 
+   
     // getchar(); 
     return 0;
 }
 
-
-// Debug 模式：很多调试信息，文件比较臃肿
-// Release 模式：去除调试信息，生成的可执行文件比较精简高效
+// Debug mode: a lot of debugging information, the file is bloated
+// Release mode: remove debugging informatoin, and the generated executable file is more streamlined and efficient
